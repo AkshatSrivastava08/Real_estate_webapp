235b224c29cf02244223c24045a16d3c1637509e
Akshat Srivastava <123894969+AkshatSrivastava08@users.noreply.github.com>
2024-12-21 17:38:44 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://real-estate-webapp-api-afyr.onrender.com",
+  baseURL: "https://localhost:8800/api",
   withCredentials: true,
 });
 
